19a843d240186cb73e9036b0488f7e2132a80efe
dev-damdam <dev.sodam@gmail.com>
2021-12-23 14:22:40 +0900

    apply vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <router-view id="app" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
-<style>
+<style lang="scss">
+html {
+  font-size: 1px;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
+}
+
+:focus {
+  outline: none;
 }
 </style>
---
+++ src/router/router.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import HmLoginPage from "../components/pages/account/hm-login-page.vue";
+import HmRegisterPage from "../components/pages/account/hm-register-page.vue";
+const routes = [
+  { path: "/", name: "root", component: HmRegisterPage },
+  { path: "/login", name: "login", component: HmLoginPage },
+  { path: "/register", name: "register", component: HmRegisterPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
