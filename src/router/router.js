855ac0d808c3e8dee6889e4f579a5f42a1c4912b
dev-damdam <dev.sodam@gmail.com>
2021-12-27 21:10:40 +0900

    impl hm-setting-page

--- src/router/router.js
+++ src/router/router.js
@@ -7,12 +7,15 @@ import HmHomePage from "../components/pages/home/hm-home-page.vue";
 
 import HmMyPage from "../components/pages/my-page/hm-my-page.vue";
 
+import HmSettingPage from "../components/pages/my-page/hm-setting-page.vue";
+
 const routes = [
   {path: "/", name: "main", component: HmHomePage},
   {path: "/login", name: "login", component: HmLoginPage},
   {path: "/register", name: "register", component: HmRegisterPage},
   {path: "/home", name: "home", component: HmHomePage},
   {path: "/my-page", name: "my-page", component: HmMyPage},
+  {path: "/setting", name: "setting", component: HmSettingPage},
 ];
 
 const router = createRouter({
