ad9be32d4c115f64cb6403e853277053c7fa4a95
dev-damdam <dev.sodam@gmail.com>
2021-12-28 21:09:45 +0900

    impl hm-edit-user-page, hm-notice-page

---
+++ src/components/pages/my-page/hm-edit-user-page.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="hm-setting-page-wrapper">
+    <hm-layout>
+      <template v-slot:header>
+        <hm-header back line l-icon="chevron-left" title="프로필 수정" @back="back" />
+      </template>
+      <div class="edit-form-wrapper">
+        <hm-input v-model="inputs.nickname" type="text" placeholder="익명의 뽀로로" :label="validateMessage.nickname" />
+        <hm-input v-model="inputs.password" type="password" placeholder="비밀번호" :label="validateMessage.password" />
+        <hm-input
+          v-model="inputs.passwordConfirm"
+          type="password"
+          placeholder="비밀번호 확인"
+          :label="validateMessage.passwordConfirm"
+        />
+      </div>
+      <hm-button type="button" @click="register" class="edit-profile-btn">프로필 수정하기</hm-button>
+    </hm-layout>
+  </div>
+</template>
+<script>
+import hmLayout from "../../layouts/hm-layout.vue";
+import hmHeader from "../../layouts/hm-header.vue";
+import hmInput from "../../elements/hm-input.vue";
+import hmButton from "../../elements/hm-button.vue";
+
+export default {
+  name: "HmEditUserPage",
+  components: {
+    hmLayout,
+    hmHeader,
+    hmInput,
+    hmButton,
+  },
+  data() {
+    return {
+      inputs: {
+        nickname: "",
+        password: "",
+        passwordConfirm: "",
+      },
+      validateMessage: {
+        nickname: "",
+        password: "",
+        passwordConfirm: "",
+      },
+    };
+  },
+  methods: {
+    back() {
+      this.$router.back();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.hm-setting-page-wrapper {
+  position: relative;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+
+  .edit-form-wrapper,
+  .edit-profile-btn {
+    margin: px(24) auto;
+    width: px(340);
+  }
+
+  .edit-profile-btn {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 10%;
+  }
+}
+</style>
---
+++ src/components/pages/setting/hm-notice-page.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="hm-setting-page-wrapper">
+    <hm-layout>
+      <template v-slot:header>
+        <hm-header back line l-icon="chevron-left" title="설정" @back="back" />
+      </template>
+      <div v-for="(notice, index) in notices" :key="index">
+        <hm-listview line @click.capture="clickNotice(index)">
+          <hm-contents align="vertical">
+            <hm-label class="sub-title">{{ notice.day }}</hm-label>
+            <hm-label class="title">{{ notice.title }}</hm-label>
+          </hm-contents>
+
+          <hm-icon :icon="!notice.isShow ? 'chevron-down' : 'chevron-up'" size="xs" />
+        </hm-listview>
+        <hm-contents class="notice-contents" v-if="notice.isShow">{{ notice.contents }}</hm-contents>
+      </div>
+    </hm-layout>
+  </div>
+</template>
+<script>
+import hmLayout from "../../layouts/hm-layout.vue";
+import hmHeader from "../../layouts/hm-header.vue";
+import hmLabel from "../../elements/hm-label.vue";
+import hmIcon from "../../elements/hm-icon.vue";
+import hmContents from "../../elements/hm-contents.vue";
+import HmListview from "../../layouts/hm-listview.vue";
+
+export default {
+  name: "HmNoticePage",
+  components: {
+    hmLayout,
+    hmHeader,
+    HmListview,
+    hmLabel,
+    hmIcon,
+    hmContents,
+  },
+  created() {
+    //test
+    for (let i = 0; i < 5; i++) {
+      this.notices.push({
+        day: "2021.09.03",
+        title: "버그 수정",
+        isShow: false,
+        contents: "tttttttt",
+      });
+    }
+  },
+  data() {
+    return {
+      notices: [],
+      selectedIndex: -1,
+    };
+  },
+  methods: {
+    back() {
+      this.$router.back();
+    },
+    clickNotice(idx) {
+      if (this.notices[idx].isShow) this.notices[idx].isShow = false;
+      else this.notices[idx].isShow = true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.hm-setting-page-wrapper {
+  position: relative;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.notice-contents {
+  width: 100%;
+  box-sizing: border-box;
+  padding: px(10);
+  border-bottom: px(1) solid $hm-gray;
+}
+</style>
